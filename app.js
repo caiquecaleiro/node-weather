4a07b2762a4590a7472ea9c29898852c56ea957f
Caique Caleiro <caiquecaleiro@gmail.com>
2016-12-26 11:44:56 -0200

    Add request to google maps api

---
+++ app.js
@@ -0,0 +1,33 @@
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+const encodedAddress = encodeURIComponent(argv.address);
+
+request({
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+  json: true
+}, (error, response, body) => {
+  if (error) {
+    console.log('Unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.');
+  } else if (body.status === 'OK') {
+    const result = body.results[0];
+    console.log(`Address: ${result.formatted_address}`);
+    console.log(`Latitude: ${result.geometry.location.lat}`);
+    console.log(`Longitude: ${result.geometry.location.lng}`);
+  }
+});
